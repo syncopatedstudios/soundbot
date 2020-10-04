dccb8951ba7bc7be31d8bc95a0fd28c058beee7e
b08x <rwpannick@gmail.com>
2020-10-04 09:54:11 -0400

    added main executable

---
+++ lib/soundbot.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+require_relative 'soundbot/config'
+
+module Soundbot
+  class Error < StandardError; end
+  #puts "#{VERSION}"
+  # load config
+  # initialize database
+end
