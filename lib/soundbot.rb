c47c4925513b1a866dfa1a818de4b90e6922b119
b08x <rwpannick@gmail.com>
2020-12-20 02:27:28 -0500

    switching to tty-prompt for menu

--- lib/soundbot.rb
+++ lib/soundbot.rb
@@ -1,6 +1,12 @@
 require "soundbot/version"
+require "soundbot/config"
+require "soundbot/cli"
+
 
 module Soundbot
   class Error < StandardError; end
   # Your code goes here...
+  puts "hey"
+
+  # @config.set(:library, value: '/home/b08x/Studio/library')
 end
--- lib/soundbot/cli.rb
+++ lib/soundbot/cli.rb
@@ -1,33 +1,18 @@
 # frozen_string_literal: true
 
-require 'thor'
+require 'tty-prompt'
 
 module Soundbot
   # Handle the application command line parsing
   # and the dispatch to various command objects
   #
   # @api public
-  class CLI < Thor
-    # Error raised by this runner
-    Error = Class.new(StandardError)
-
-    desc 'version', 'soundbot version'
-    def version
-      require_relative 'version'
-      puts "v#{Soundbot::VERSION}"
+  class CLI
+    def self.start
+      prompt = TTY::Prompt.new
+      prompt.say("hey")
     end
-    map %w(--version -v) => :version
 
-    desc "config", "Command description..."
-    method_option :help, aliases: "-h", type: :boolean,
-                         desc: "Display usage information"
-    def config(*)
-      if options[:help]
-        invoke :help, ["config"]
-      else
-        require_relative "commands/config"
-        Soundbot::Commands::Config.new(options).execute
-      end
-    end
   end
+
 end
