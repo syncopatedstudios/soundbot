df646442f902199414bff1fec6516fa5ecfedc5a
b08x <rwpannick@gmail.com>
2021-01-01 08:16:27 -0500

    added functions to set directory and to initialize db connector

--- lib/soundbot.rb
+++ lib/soundbot.rb
@@ -1,9 +1,28 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'sequel'
+
 require "soundbot/version"
 require "soundbot/config"
 require "soundbot/database"
 
 module Soundbot
   class Error < StandardError; end
+
+    def self.config_dir
+      File.join(Dir.home, '.config', 'soundbot')
+    end
+
+    def self.studio_dir
+      File.join(Dir.home, 'Studio')
+    end
+
+    def self.recordings_dir
+      File.join(studio_dir, 'recordings')
+    end
+
 end
 
-puts "hey"
+puts "or...git@github.com:YellowApple/bales.git":
+puts "hey...https://cmdparse.gettalong.org/tutorial.html"
--- lib/soundbot/database.rb
+++ lib/soundbot/database.rb
@@ -1,2 +1,11 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+
+module Soundbot
+  class Database
+    def initialize
+      @db = Sequel::Database.new
+    end
+  
+  end
+end
